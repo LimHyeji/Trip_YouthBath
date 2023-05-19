6a68f73c5c75cab6c88986ab9e8e7eb8eb86e51c
LimHyeji <87571953+LimHyeji@users.noreply.github.com>
2023-05-19 11:40:14 +0900

    create sql query

---
+++ backend/src/main/resources/DB/main.sql
@@ -0,0 +1,11 @@
+create database huhjdb;
+
+use huhjdb;
+
+create table member(
+    id varchar(20) primary key,
+    pw varchar(500),
+    name varchar(20)
+    );
+
+# 관광지 크롤링 코드 실행
---
+++ backend/src/main/resources/DB/sec.sql
@@ -0,0 +1,8 @@
+create database secdb;
+
+use secdb;
+
+create table secMember(
+    id varchar(20),
+    salt varchar(500)
+);
